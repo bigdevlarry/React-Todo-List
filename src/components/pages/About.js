fa58989c7ab0f6e3b02bc52b1ae9f91677433734
larrysul <suleabimbola@gmail.com>
2020-05-04 15:43:24 +0100

    React TodoList

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <React.Fragment>
             <h1>About </h1>
-            <p>This is my First React Build </p>
+            <p>This is my First React Build, tutored by Brad Traversey & Mentored by Even  </p>
         </React.Fragment>
     )
 }
